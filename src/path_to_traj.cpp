acc86fdb8267d8eccaec7fb83fd4581ae892560f
v4hn <me@v4hn.de>
2022-05-18 23:19:39 +0200

    late-night commit
    probably a lot of issues and bad ideas...
    I already see the first ones committing :P

--- src/path_to_traj.cpp
+++ src/path_to_traj.cpp
@@ -5,20 +5,48 @@
 #include <nav_msgs/Path.h>
 #include <moveit_msgs/DisplayTrajectory.h>
 
+struct GenerateArgs {
+	trajectory_msgs::JointTrajectory& trajectory;
+	const nav_msgs::Path path;
+	const std::string group;
+	const std::string tip;
+	const planning_scene::PlanningScene& scene;
+};
+
+void generateTrajectory(const GenerateArgs& args){
+	auto& traj{ args.trajectory };
+	traj.header.stamp = ros::Time::now();
+
+	const auto* jmg{ args.scene.getRobotModel()->getJointModelGroup(args.group) };
+	if(!jmg){
+		ROS_FATAL_STREAM("specified group '" << args.group << "' does not exist");
+		return;
+	}
+	traj.joint_names = jmg->getActiveJointModelNames();
+
+	traj.points.emplace_back();
+	args.scene.getCurrentState().copyJointGroupPositions(
+	         jmg,
+	         traj.points.back().positions);
+	traj.points.back().time_from_start= ros::Duration(0.0);
+
+	for(std::size_t i = 0; i < traj.points.front().positions.size(); ++i){
+		traj.points.emplace_back(traj.points.back());
+		traj.points.back().positions[i] = 0.0;
+		traj.points.back().time_from_start= ros::Duration(0.5);
+	}
+}
+
 int main(int argc, char** argv){
    ros::init(argc, argv, "path_to_traj");
    ros::NodeHandle nh, pnh{"~"};
 
-   ros::AsyncSpinner spinner{ 1 };
+	ros::AsyncSpinner spinner{ 2 };
    spinner.start();
 
 	ros::Publisher pub_traj { nh.advertise<moveit_msgs::DisplayTrajectory>("pluck/trajectory", 1, true) };
 
-   planning_scene_monitor::PlanningSceneMonitor psm{ "robot_description" };
-   if(!psm.requestPlanningSceneState()){
-      ROS_FATAL_STREAM("failed to get current scene from move_group");
-      return 1;
-   }
+	planning_scene_monitor::PlanningSceneMonitor psm{ "robot_description" };
 	auto& scene { *psm.getPlanningScene() };
    ROS_INFO("finished loading scene");
 
@@ -27,37 +55,28 @@ int main(int argc, char** argv){
    std::string tip_name;
    pnh.param<std::string>("tip", tip_name, "rh_palm");
 
-	nav_msgs::PathConstPtr path{ ros::topic::waitForMessage<nav_msgs::Path>("pluck/path", nh) };
-	ROS_INFO_STREAM("got path with " << path->poses.size() << " poses");
+	ros::Subscriber sub{ nh.subscribe<nav_msgs::Path>("pluck/path", 1,
+		                                               [&](auto& path){
+		ROS_INFO_STREAM("got path with " << path->poses.size() << " poses");
 
-	moveit_msgs::DisplayTrajectory trajectory;
-	trajectory.model_id = psm.getRobotModel()->getName();
-	trajectory.trajectory.resize(1);
-	moveit::core::robotStateToRobotStateMsg(scene.getCurrentState(), trajectory.trajectory_start, false);
-
-	auto& jtrajectory{ trajectory.trajectory[0].joint_trajectory };
-	jtrajectory.header.stamp = ros::Time::now();
-   const auto* jmg{ psm.getRobotModel()->getJointModelGroup(group_name) };
-   if(!jmg){
-      ROS_FATAL_STREAM("specified group '" << group_name << "' does not exist");
-      return 1;
-   }
-	jtrajectory.joint_names = jmg->getActiveJointModelNames();
-
-	jtrajectory.points.emplace_back();
-	scene.getCurrentState().copyJointGroupPositions(
-	         jmg,
-	         jtrajectory.points.back().positions);
-	jtrajectory.points.back().time_from_start= ros::Duration(0.0);
-
-	for(std::size_t i = 0; i < jtrajectory.points.front().positions.size(); ++i){
-		jtrajectory.points.emplace_back(jtrajectory.points.back());
-		jtrajectory.points.back().positions[i] = 0.0;
-		jtrajectory.points.back().time_from_start= ros::Duration(0.5);
-	}
-
-	pub_traj.publish(trajectory);
-	ROS_INFO_STREAM("publish trajectory with " << trajectory.trajectory[0].joint_trajectory.points.size() << " points");
+		moveit_msgs::DisplayTrajectory trajectory;
+		trajectory.model_id = psm.getRobotModel()->getName();
+		trajectory.trajectory.resize(1);
+		if(!psm.requestPlanningSceneState()){
+			ROS_ERROR_STREAM("failed to get current scene from move_group");
+			return;
+		}
+		moveit::core::robotStateToRobotStateMsg(scene.getCurrentState(), trajectory.trajectory_start, false);
+		generateTrajectory({
+		   .trajectory = trajectory.trajectory[0].joint_trajectory,
+		   .path = *path,
+		   .group = group_name,
+		   .tip = tip_name,
+		   .scene = scene});
+		pub_traj.publish(trajectory);
+		ROS_INFO_STREAM("publish trajectory with " << trajectory.trajectory[0].joint_trajectory.points.size() << " points");
+		})
+	};
 
 
 	ros::waitForShutdown();
