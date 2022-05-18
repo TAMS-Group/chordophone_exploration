d6429450dc3077b8cc55041246f5d301e07e23c0
v4hn <me@v4hn.de>
2022-05-18 21:57:31 +0200

    syntax fixup

--- scripts/pluck_path.py
+++ scripts/pluck_path.py
@@ -7,7 +7,7 @@ from geometry_msgs.msg import PoseStamped
 
 def main():
     rospy.init_node('publish_path')
-    pub = rospy.Publisher('pluck/path', Path, latch=True, queue_size= 1)
+    pub = rospy.Publisher('pluck/path', Path, latch=True, queue_size=1)
 
     path = Path()
     path.header.frame_id = 'guzheng/d6/head'
@@ -18,9 +18,9 @@ def main():
         (.08, -0.03, 0.00),
         ]
 
-    for x,y,z in waypoints:
+    for x, y, z in waypoints:
         p = PoseStamped()
-        p.pose.position.x = x # 0 - 0.15
+        p.pose.position.x = x  # 0 - 0.15
         p.pose.position.y = y
         p.pose.position.z = z
         p.pose.orientation.w = 1.0
