c90bd3dff4790ef3b7904c499cac8a15a7cde9fe
v4hn <me@v4hn.de>
2022-05-30 19:54:10 +0000

    fix alternate poses script after config changes

--- src/alternate_poses.cpp
+++ src/alternate_poses.cpp
@@ -9,20 +9,21 @@ int main(int argc, char** argv){
   spinner.start();
   ros::NodeHandle nh{"~"};
 
-  moveit::planning_interface::MoveGroupInterface mgi{ "right_arm" };
+  moveit::planning_interface::MoveGroupInterface mgi{ "right_arm_pr2" };
   mgi.setGoalTolerance(0.0);
 
   double factor{ nh.param("factor", 0.1) };
   mgi.setMaxVelocityScalingFactor(factor);
   mgi.setMaxAccelerationScalingFactor(factor);
 
-//  mgi.rememberJointValues("extended", {0.01400007014833582, -0.1503130120423125, -0.10460533795017679, -0.8218330893876233, -1.5417190468481798});
-//  mgi.rememberJointValues("side", {-1.4732617386197226, 0.8506141181302039, 0.0538252305033029, -2.1131878349293762, 0.03634948516335991});
+  mgi.rememberJointValues("extended", {0.01400007014833582, -0.1503130120423125, -0.10460533795017679, -0.8218330893876233, -1.5417190468481798});
+  mgi.rememberJointValues("side", {-1.4732617386197226, 0.8506141181302039, 0.0538252305033029, -2.1131878349293762, 0.03634948516335991});
 
   mgi.rememberJointValues("back", {-1.1988408351678985, 1.0235261409558147, -0.9605794011128038, -1.8188689786865082, -1.1118573437184969});
   mgi.rememberJointValues("front", {-1.0987725480179433, 0.9397771670823535, -0.8969185350439461, -1.7509715150475975, -1.0414574388996534});
 
-//  while(ros::ok())
+  while(ros::ok())
+//    for(auto&& target : {"extended", "side"}){
     for(auto&& target : {"back", "front"}){
       ROS_INFO_STREAM("planning path to " << target);
 
