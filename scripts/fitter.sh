5e019b6665175a34f724ab0292e46684cec462d8
v4hn <me@v4hn.de>
2023-04-26 19:49:13 +0000

    helper script to toggle fitter state

---
+++ scripts/fitter.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+if [ "$1" = "on" ]; then
+    rosservice call /string_fitter/set_active "data: true"
+elif [ "$1" = "off" ]; then
+    rosservice call /string_fitter/set_active "data: false"
+else
+    echo "Usage: $0 <on/off>"
+    exit 1
+fi
